dedc376c06acfaf84efac9a8c9206e7b642989d6
gokul raman <sgokulraman5@gmail.com>
2026-01-08 22:50:25 +0530

    Key change

--- projects/settings.py
+++ projects/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
-
+import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rdgmp5()9!*!w))q!r3%(3+6i8d=+9k!hl0=0un1#92f=j&axr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',case=bool)
 
 ALLOWED_HOSTS = []
 
